61548c2089ab4c64847fd7782e06f57925176103
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-11 18:09:37 +0200

    feat: add filter to search an actor/actress by name adding query-string to url

--- src/App.jsx
+++ src/App.jsx
@@ -17,28 +17,61 @@ function App() {
   //creo una variabile per la lista di attori completa
   const [unifiedListActors, setUnifiedListActors] = useState([]);
 
+  //creo una variabile dove salvo il valore della ricerca, impostato inizialmente vuoto
+  const [search, setSearch] = useState('');
+
   function unifiedList() {
-    axios.get(urlActresses)
-      .then(responseActresses => {
-        axios.get(urlActors)
-          .then(responseActors => {
-            //salvo in delle variabili il valore delle liste relative alle attrici e agli attori presi dall'oggetto ottenuto in risposta
-            const responseActressesList = responseActresses.data;
-            const responseActorsList = responseActors.data;
+    if (search === '') {
+      axios.get(urlActresses)
+        .then(responseActresses => {
+          axios.get(urlActors)
+            .then(responseActors => {
+              //salvo in delle variabili il valore delle liste relative alle attrici e agli attori presi dall'oggetto ottenuto in risposta
+              const responseActressesList = responseActresses.data;
+              const responseActorsList = responseActors.data;
 
-            //creo un unico array con le liste degli attori
-            const completeList = [...responseActressesList, ...responseActorsList];
+              //creo un unico array con le liste degli attori
+              const completeList = [...responseActressesList, ...responseActorsList];
 
-            //vado a settare come array principale, l'array unico creato
-            setUnifiedListActors(completeList);
-          })
-      })
+              //vado a settare come array principale, l'array unico creato
+              setUnifiedListActors(completeList);
+            })
+        })
+    } else {
+      //creo una variabile che sarà aggiunta al mio url di partenza che identificherà la querystring per permettere la ricerca per nome
+      const queryString = `?search=${search}`;
+
+      axios.get(urlActresses + queryString)
+        .then(responseActresses => {
+          axios.get(urlActors + queryString)
+            .then(responseActors => {
+
+              const responseActressesList = responseActresses.data;
+              const responseActorsList = responseActors.data;
+
+              //creo un array con le liste degli attori risultanti dalla ricerca
+              const filteredList = [...responseActressesList, ...responseActorsList];
+
+              //vado a settare come array principale, l'array unico creato
+              setUnifiedListActors(filteredList);
+            })
+        })
+    }
+
   }
 
-  useEffect(unifiedList, []);
+  //aggiungo come dipendenza il valore di search in modo tale che se verrà fatta una ricerca avrò un render della pagina
+  useEffect(unifiedList, [search]);
+
   return (
     <>
       <h1>Attrici e attori più famosi di Hollywood</h1>
+
+      <input type="text"
+        placeholder="Cerca per nome..."
+        value={search}
+        onChange={event => setSearch(event.target.value)} />
+
       <main>
         {unifiedListActors.map((element, index) => <Card key={index} name={element.name}>
           <li>Nome: {element.name}</li>

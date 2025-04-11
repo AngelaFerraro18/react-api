6dc280b55abea4d15acd12287961aba4332ac8dd
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-11 17:25:53 +0200

    feat: h1 modified to describe a complete actors list (male and female), add an other API to add actors to the list

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,13 @@ import Card from "./components/Card";
 //salvo in una variabile l'url dell'API che ho scelto (attrici)
 const urlActresses = 'https://www.freetestapi.com/api/v1/actresses';
 
+//salvo in una variabile l'url dell'API degli attori
+const urlActors = 'https://www.freetestapi.com/api/v1/actors';
+
 function App() {
 
   const [actresses, setActresses] = useState([]);
+  const [actors, setActors] = useState([]);
 
   function fetchActresses() {
     axios.get(urlActresses)
@@ -21,11 +25,23 @@ function App() {
       })
   }
 
-  useEffect(fetchActresses, []);
+  function fetchActors() {
+    axios.get(urlActors)
+      .then(response => {
+        console.log(response.data);
+        return setActors(response.data);
+      })
+  }
+
+  //per scrivere entrambe le funzioni uso la callback di useEffect
+  useEffect(() => {
+    fetchActors(),
+      fetchActresses()
+  }, []);
 
   return (
     <>
-      <h1>Attrici più famose di Hollywood</h1>
+      <h1>Attrici e attori più famosi di Hollywood</h1>
       <main>
         {actresses.map(actress => <Card key={actress.id} name={actress.name}>
           <li>Nome: {actress.name}</li>
@@ -35,6 +51,17 @@ function App() {
           <li>Riconoscimenti: {actress.awards}</li>
           <div>
             <img src={actress.image} alt={actress.name} />
+          </div>
+        </Card>)}
+
+        {actors.map(actor => <Card key={actor.id} name={actor.name}>
+          <li>Nome: {actor.name}</li>
+          <li>Anno di nascita: {actor.birth_year}</li>
+          <li>Nazionalità: {actor.nationality}</li>
+          <li>Biografia: {actor.biography}</li>
+          <li>Riconoscimenti: {actor.awards}</li>
+          <div>
+            <img src={actor.image} alt={actor.name} />
           </div>
         </Card>)}
       </main>

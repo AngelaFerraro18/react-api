b3003b09b8871a42387777dc8fc4e1331992a283
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-11 14:35:51 +0200

    feat: milestone 1 completed --> actresses list in console

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+//importo axios
+import axios from "axios"
+//importo useEffect
+import { useEffect } from "react";
+
+
+//salvo in una variabile l'url dell'API che ho scelto (attrici)
+const urlActresses = 'https://www.freetestapi.com/api/v1/actresses';
 
 function App() {
 
+  function fetchActresses() {
+    axios.get(urlActresses)
+      .then(response => console.log(response.data))
+  }
+
+  useEffect(fetchActresses, []);
+
   return (
     <>
-      Ciao sono in app
+
     </>
   )
 }
